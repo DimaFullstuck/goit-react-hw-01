dca6d470c444dc8973ab01a0815ae355c1841276
Dima <dima1999ostapenko@gmail.com>
2024-09-12 20:15:19 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -6,8 +6,8 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
       <img
         className={s.avatar}
         src={avatar}
-        alt={`${name}'s avatar`}
-        width="48"
+        alt={`${name}'s avatar'`}
+        width={"48"}
       />
       <p className={s.name}>{name}</p>
       <p className={clsx(isOnline ? s.isOnline : s.isOffline)}>
